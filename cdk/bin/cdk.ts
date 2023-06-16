b0a19b95f51f06e8d7b37d05e303ca750799b8c3
Kyle Chang <Kyle.Chang@acer.com>
2023-06-16 18:09:33 +0000

    deploy EC2 after deploying VPC

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -9,32 +9,27 @@ dotenv.config({ path: process.env.MYSELF_ENV_PATH })
 
 const app = new cdk.App();
 
-const vpcStack = new VpcStack(app, 'VpcStack', {
+// const vpcStack = new VpcStack(app, 'VpcStack', {
+//   env: {
+//     account: process.env.AWS_ACCOUNT_ID,
+//     region: process.env.AWS_REGION
+//   }
+// });
+
+// cdk.Tags.of(vpcStack).add('owner', 'changyuchun159630@gmail.com')
+// cdk.Tags.of(vpcStack).add('deploy', 'cdk')
+
+const ec2Stack = new Ec2Stack(app, 'Ec2Stack', {
   env: {
     account: process.env.AWS_ACCOUNT_ID,
     region: process.env.AWS_REGION
   },
   tags: {
-    vpcId: 'vpc-022060a8b156e641e',
+    vpcId: 'vpc-0c15e20785c390709',
     amiId: 'ami-03f65b8614a860c29',
     keyPairName: 'myKeypair'
   }
 });
 
-cdk.Tags.of(vpcStack).add('owner', 'changyuchun159630@gmail.com')
-cdk.Tags.of(vpcStack).add('deploy', 'cdk')
-
-// const ec2Stack = new Ec2Stack(app, 'Ec2Stack', {
-//   env: {
-//     account: process.env.AWS_ACCOUNT_ID,
-//     region: process.env.AWS_REGION
-//   },
-//   tags: {
-//     vpcId: 'vpc-022060a8b156e641e',
-//     amiId: 'ami-03f65b8614a860c29',
-//     keyPairName: 'myKeypair'
-//   }
-// });
-
-// cdk.Tags.of(ec2Stack).add('owner', 'changyuchun159630@gmail.com')
-// cdk.Tags.of(ec2Stack).add('deploy', 'cdk')
+cdk.Tags.of(ec2Stack).add('owner', 'changyuchun159630@gmail.com')
+cdk.Tags.of(ec2Stack).add('deploy', 'cdk')
