a839d67084a5d9af2e5b2bdf9c94bb1515c38464
Yu Chun Chang <changyuchun159630@gmail.com>
2024-02-11 19:42:22 +0000

    remove dotenv

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,9 +3,6 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EC2Stack } from '../lib/ec2';
-import * as dotenv from 'dotenv'
-
-dotenv.config({ path: process.env.ENV_PATH })
 
 const app = new cdk.App();
 
